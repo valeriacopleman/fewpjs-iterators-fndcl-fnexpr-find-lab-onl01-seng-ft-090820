d96d09adf1b03d85e99c139e97215bc8da1efac0
Valeria Copleman <valeriacopleman@gmail.com>
2021-01-01 13:17:26 -0800

    commit tree ec9b5e4b1bca5cc7e94ac63a3650c2ef02ed9f6b

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record) {
+  const desiredResult = record.find(({ result }) => result === 'W');
+  
+  return (desiredResult ? desiredResult.year : undefined)
+}
